9fee37e61b536f414e17aa2eb6202a60355bae00
Anugrah Premachandran <158727858+an-gr-hh1@users.noreply.github.com>
2024-06-08 15:53:14 +0530

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+const sendEmail= () => {
+    window.location = "https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=anugrahempire01@gmail.com";
+}
+{/*https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=someone@example.com*/}
+{/*"mailto:anugrahempire01@gmail.com"*/}
